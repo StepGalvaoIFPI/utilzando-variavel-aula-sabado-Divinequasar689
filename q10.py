3f89daebb4933f13fab8b59cc86656a5fd15fa06
jthur0411B <51513857+Divinequasar689@users.noreply.github.com>
2025-04-16 14:44:36 -0300

    Update q10.py

--- q10.py
+++ q10.py
@@ -1,1 +1,7 @@
-
+salariobruto_fulano=5000
+inss=salariobruto_fulano*0.15
+ir=salariobruto_fulano*0.10
+impostototal=inss+ir
+salarioliquidofulano=salariobruto_fulano-impostototal
+print(salarioliquidofulano)
+3750.0
